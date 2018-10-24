933ffbcb4b3bf617a00f8c7dfb3727794ef0579a
Bryan Carranza <bryancarranza@bryans-macbook-pro.local>
2018-10-24 18:00:38 -0500

    Initial Commit

---
+++ number 2 chapter 12/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  number 2 chapter 12
+//
+//  Created by Bryan Carranza on 10/24/18.
+//  Copyright © 2018 Bryan Carranza. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
